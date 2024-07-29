f8124083f986354068ffd0831ed5a9fc7decafa5
Sagar Lodhe <sagar1990ku@gmail.com>
2024-07-29 22:38:50 +0530

    Update CORS

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://master--sagarapp.netlify.app/",
     // origin:'*',
     credentials: true,
   })
